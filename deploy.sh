6e209eafcdc492f565dbcd61cd6d9a6ae38c4a75
mcsrainbow <guosuiyu@gmail.com>
2023-06-06 23:53:00 +0800

    Update script deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,32 +1,33 @@
 #!/bin/bash
 # Build Hugo site and deploy it to main branch
 
-if [[ $(git status -s) ]]; then
-  echo "The working directory is dirty, please commit all pending changes."
-  exit 1;
-fi
-
+tput_red=$(tput setaf 1)
 tput_green=$(tput setaf 2)
 tput_reset=$(tput sgr0)
 
-echo -e "${tput_green}0.Deploying updates to GitHub...${tput_reset}"
+if [[ $(git status -s) ]]; then
+  echo -e "${tput_red}ERROR: The working directory is dirty, please commit all pending changes.${tput_reset}"
+  exit 1;
+fi
 
-echo -e "${tput_green}1.Deleting old public folder...${tput_reset}"
+echo -e "${tput_green}INFO: 0.Deploying updates to GitHub...${tput_reset}"
+
+echo -e "${tput_green}INFO: 1.Deleting old public folder...${tput_reset}"
 rm -rf public
 mkdir public
 git worktree prune
 rm -rf .git/worktrees/public/
 
-echo -e "${tput_green}2.Checking out main branch into public...${tput_reset}"
+echo -e "${tput_green}INFO: 2.Checking out main branch into public...${tput_reset}"
 git worktree add -B main public origin/main
 
-echo -e "${tput_green}3.Removing existing files in public folder...${tput_reset}"
+echo -e "${tput_green}INFO: 3.Removing existing files in public folder...${tput_reset}"
 rm -rf public/*
 
-echo -e "${tput_green}4.Generating blog...${tput_reset}"
+echo -e "${tput_green}INFO: 4.Generating blog...${tput_reset}"
 hugo
 
-echo -e "${tput_green}5.Updating main branch...${tput_reset}"
+echo -e "${tput_green}INFO: 5.Updating main branch...${tput_reset}"
 cd public && git add --all 
 
 msg="Rebuild blog on $(date '+%b %d, %Y')"
@@ -39,4 +40,4 @@ git push -f origin main
 
 cd ..
 
-echo -e "${tput_green}6.Done${tput_reset}"
+echo -e "${tput_green}INFO: 6.Done${tput_reset}"
