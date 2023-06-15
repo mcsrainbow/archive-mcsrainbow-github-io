a48072dd581ce76a852747b66f0db89a07234a15
mcsrainbow <guosuiyu@gmail.com>
2023-06-15 09:06:21 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 # Build Hugo site and deploy it to main branch
 
-tput_red=$(tput setaf 1)
-tput_green=$(tput setaf 2)
-tput_reset=$(tput sgr0)
+# Readline library accepts \001 and \002 as non-visible text delimiters
+# The bash-specific \[ and \] are translated to \001 and \002
+nvt_open=$'\001' # non-visible text open
+nvt_close=$'\002' # non-visible text close
+
+# Enable color in terminal with tput and non-visible text delimiters
+tput_red=${nvt_open}$(tput setaf 1)${nvt_close}
+tput_green=${nvt_open}$(tput setaf 2)${nvt_close}
+tput_reset=${nvt_open}$(tput sgr0)${nvt_close}
 
 if [[ $(git status -s) ]]; then
   echo -e "${tput_red}ERROR: The working directory is dirty, please commit all pending changes.${tput_reset}"
